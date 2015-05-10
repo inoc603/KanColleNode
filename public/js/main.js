8c5589849e7f77f94e91d733462916c9e9081ae4
Eddie Huang <inoc603@gmail.com>
2015-05-11 01:20:00 +0900

    add touchspin and datatables update exp calculator

--- public/js/main.js
+++ public/js/main.js
@@ -9,12 +9,15 @@ require.config(
          , 'bootstrap-contextmenu': 'vendor/bootstrap-contextmenu'
          , 'bootstrap-select': 'vendor/bootstrap-select'
          , 'bootstrap-formhelpers': 'vendor/bootstrap-formhelpers'
+         , 'data-tables': 'vendor/jquery.dataTables'
+         , 'bootstrap-touchspin': 'vendor/jquery.bootstrap-touchspin'
          }
 , shim: { 'socket.io': { exports: 'io' }
         , 'bootstrap': ['jquery']
         , 'jquery.ui': ['jquery']
         , 'bootstrap-select': ['jquery', 'bootstrap']
         , 'bootstrap-formhelpers': ['jquery']
+        , 'data-tables': ['jquery']
         }
 })
 
--- public/js/views/panels/exp.js
+++ public/js/views/panels/exp.js
@@ -7,6 +7,7 @@ define(
   , 'apps/updaters/fleet'
   , 'bootstrap-select'
   , 'bootstrap-formhelpers'
+  , 'bootstrap-touchspin'
   ]
 , function ( $, _, Backbone, expTpl, fleetUpdater) {
     var fleetPanelView = Backbone.View.extend({
@@ -17,6 +18,32 @@ define(
                                          , template: expTpl})
         this.model.render(display)
         fleetUpdater.initialize()
+        this.addTouchSpin()
+        // $('.selectpicker.select-wide').selectpicker({size: 5})
+        // $('.selectpicker.select-narrow').selectpicker({width: '50px'})
+        $('.selectpicker').selectpicker()
+        // $('button[title="Nothing selected"]>span:first').text('请更新舰娘列表')
+      },
+      addTouchSpin: function () {
+        $('input#exp-now-level').TouchSpin({
+          min: 1,
+          max: 150,
+          step: 1,
+          decimals: 0,
+          prefix: '当前',
+          postfix: '级',
+          maxboostedstep: 10
+        })
+
+        $('input#exp-target-level').TouchSpin({
+          min: 1,
+          max: 150,
+          step: 1,
+          decimals: 0,
+          prefix: '目标',
+          postfix: '级',
+          maxboostedstep: 10
+        })
       }
     })
     return fleetPanelView
